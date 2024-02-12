6fcc87f4be63812a4ceff94d326f0b3b807e2f2b
Robert Bailey <r.bail@hotmail.com>
2024-02-13 04:41:23 +1030

    app starting

---
+++ server/utils/auth.js
@@ -0,0 +1,19 @@
+const { GraphQLError } = require('graphql');
+const jwt = require('jsonwebtoken');
+
+// set token secret and expiration date
+const secret = 'mysecretsshhhhh';
+const expiration = '2h';
+
+module.exports = {
+  AuthenticationError: new GraphQLError('Could not authenticate user.', {
+    extensions: {
+      code: 'UNAUTHENTICATED',
+    },
+  }),
+  signToken: function ({ email, username, _id }) {
+    const payload = { email, username, _id };
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+};
+
