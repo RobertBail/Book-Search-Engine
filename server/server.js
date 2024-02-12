4e7fd649a1def850a6947b8201f2010ccac5d937
Robert Bailey <r.bail@hotmail.com>
2024-02-13 05:52:10 +1030

    trying to deploy

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
+const routes = require('./routes');
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
@@ -13,7 +14,7 @@ const server = new ApolloServer({
   resolvers,
   context: authMiddleware,
 });
-//server.applyMiddleware({ app });
+server.applyMiddleware({ app });
 const app = express();
 
 // Create a new instance of an Apollo server with the GraphQL schema
@@ -28,10 +29,11 @@ const startApolloServer = async () => {
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
 
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/build/index.html'));
-    });
+    //app.get('*', (req, res) => {
+      //res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    //});
   } 
+  app.use(routes);
 
   db.once('open', () => {
     app.listen(PORT, () => {
